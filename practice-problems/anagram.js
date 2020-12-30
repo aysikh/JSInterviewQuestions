00c9d99f8ebb8102726023a25916fd7a3f363605
aysikh <amy.sikhammountry@gmail.com>
2020-12-30 11:26:27 -0500

    anagram

---
+++ practice-problems/anagram.js
@@ -0,0 +1,11 @@
+/**
+ * Write a function to determine if one String is 
+ * a case-insensitive anagram of another String
+ */
+
+let isAnagram = function (str1, str2) {
+
+}
+
+console.log(isAnagram('hello', 'jello'));
+console.log(isAnagram('hello', 'loelh'));
