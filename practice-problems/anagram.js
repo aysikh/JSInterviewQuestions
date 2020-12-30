58307d4215613e1ea46db7952aa22d2bfe16a54c
aysikh <amy.sikhammountry@gmail.com>
2020-12-30 11:29:55 -0500

    anagram solution

--- practice-problems/anagram.js
+++ practice-problems/anagram.js
@@ -4,7 +4,9 @@
  */
 
 let isAnagram = function (str1, str2) {
-
+  let sorted1 = str1.split('').sort().join('').toLowerCase();
+  let sorted2 = str2.split('').sort().join('').toLowerCase();
+  return (sorted1 === sorted2);
 }
 
 console.log(isAnagram('hello', 'jello'));
