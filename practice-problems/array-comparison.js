9e03adf867b896972640c6cfd544dd04222752c4
aysikh <amy.sikhammountry@gmail.com>
2021-01-01 19:46:08 -0500

    array comparing

---
+++ practice-problems/array-comparison.js
@@ -0,0 +1,10 @@
+/**
+ * Which of the following lines will return a true and
+ * which will return false?
+ */
+
+console.log(["a", "b"] == ["a", "b"]);
+console.log(["a", "b"] == ["a", "b"] + []);
+console.log(["a", "b", "c"] == ["a", "b"] + []);
+console.log(["a", "b"] == ["b", "a"]);
+console.log(["a", "b"] == "a,b");
---
+++ practice-problems/unary-plus.js
@@ -0,0 +1,6 @@
+/**
+ * What will be the value stored in the variable n?
+ *
+ */
+
+let n = ((x = 1), (y = 2), (z = 3));
