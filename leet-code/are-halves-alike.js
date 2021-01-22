10947369d06bee0386be53ea12af5424ecb608b0
aysikh <amy.sikhammountry@gmail.com>
2021-01-22 11:45:28 -0500

    are halves alike

---
+++ leet-code/are-halves-alike.js
@@ -0,0 +1,14 @@
+// ////;/;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;/
+
+
+
+const halvesAreAlike = s => {
+  const target = new Set(["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]);
+  let a = 0, b = 0;
+  for (let left = 0, right = s.length - 1; left < right; ++left, --right) {
+    target.has(s[left]) && ++a;
+    target.has(s[right]) && ++b;
+  }
+  return a === b;
+};
+
