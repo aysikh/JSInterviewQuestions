e32b6e047198f8342e7e8db830ab7f9778015e22
aysikh <amy.sikhammountry@gmail.com>
2021-01-23 23:48:38 -0500

    solution

--- leet-code/greatest-elements-on-right.js
+++ leet-code/greatest-elements-on-right.js
@@ -20,3 +20,15 @@
 // Input: arr = [400]
 // Output: [-1]
 // Explanation: There are no elements to the right of index 0.
+
+
+const replaceElements = arr => {
+  const result = new Array(arr.length);
+  result[arr.length - 1] = -1;
+  
+  for (let i = arr.length - 1; i > 0; i -= 1) {
+      result[i - 1] = Math.max(arr[i], result[i]);
+  }
+  
+  return result;
+};
