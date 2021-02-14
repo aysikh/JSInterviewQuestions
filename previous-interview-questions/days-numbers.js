a955e1ba3820aced90826464e93d123afbb20f1a
aysikh <amy.sikhammountry@gmail.com>
2021-02-14 01:44:35 -0500

    solution to updated day

--- previous-interview-questions/days-numbers.js
+++ previous-interview-questions/days-numbers.js
@@ -1,3 +1,15 @@
+
 function whatDay(day, number){
-  
+  let days = ["Mon", "Tues", "Weds", "Thurs", "Fri", "Sat", "Sun"]
+  // get the index number from each day 
+  // add number to day 
+  // module % 7 
+  // return the day 
+  let daysIndex = days.indexOf(day)
+  // console.log(daysIndex) 
+  let newDayIndex = daysIndex + number % 7 
+  // console.log(newDayIndex) 
+  return days[newDayIndex]
 }
+
+console.log(whatDay("Weds", 1))
